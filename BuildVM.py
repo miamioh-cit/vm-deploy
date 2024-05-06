2f5211131741d027af6ea63f8225f983af7fac1c
Hunterds <105031083+hunterds-miamioh@users.noreply.github.com>
2024-05-06 19:15:39 -0400

    Update BuildVM.py

--- BuildVM.py
+++ BuildVM.py
@@ -305,4 +305,4 @@ def main(vcenter_ip, vcenter_user, vcenter_password, class_option, datacenter_na
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host=‘0.0.0.0’, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
