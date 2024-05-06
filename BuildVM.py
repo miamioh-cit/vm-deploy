e3ab3f87beb38d304b9c872b68208e268c9586eb
Hunterds <105031083+hunterds-miamioh@users.noreply.github.com>
2024-05-06 19:12:01 -0400

    Update BuildVM.py

--- BuildVM.py
+++ BuildVM.py
@@ -304,5 +304,5 @@ def main(vcenter_ip, vcenter_user, vcenter_password, class_option, datacenter_na
     return ip_address  # Return the IP address or None if the operation was unsuccessful
 
 if __name__ == "__main__":
-    port = int(os.environ.get(“PORT”, 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host=‘0.0.0.0’, port=port)
